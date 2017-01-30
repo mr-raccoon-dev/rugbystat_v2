9424254c9bfd28e81d2a253dce22daaed651b23b
krnr <serova_s@land.ru>
2017-01-30 20:53:44 +0300

    Log creating lock file for import_from_dropbox

--- rugbystat/clippings/management/commands/check_dropbox.py
+++ rugbystat/clippings/management/commands/check_dropbox.py
@@ -1,4 +1,5 @@
 import os
+import logging
 
 from django.conf import settings
 from django.core.management import BaseCommand
@@ -6,13 +7,18 @@ from django.core.management import BaseCommand
 from clippings.utils import process_user
 
 
+logger = logging.getLogger('django.request')
+
+
 class Command(BaseCommand):
     def handle(self, *args, **options):
         os.chdir(settings.BASE_DIR)
         locks = [file for file in os.listdir() if '.lock' in file]
+        logger.debug('Got following locks in dir: {}'.format(locks))
         if locks:
             # grab the first lock if exists
             filename = locks[0] 
             uid = int(filename.split('.')[0])
             process_user(uid)
+            logger.debug('Removing lock {}'.format(filename))
             os.remove(filename)
--- rugbystat/clippings/views.py
+++ rugbystat/clippings/views.py
@@ -1,10 +1,14 @@
 import json
+import logging
 
+from django.conf import settings
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 
 from .utils import validate_request, process_user
 
+
+logger = logging.getLogger('django.request')
 
 @csrf_exempt
 def import_from_dropbox(request):
@@ -16,10 +20,15 @@ def import_from_dropbox(request):
             return HttpResponse('False', content_type="text/plain")
 
         req = json.loads(request.body.decode('UTF-8'))
+        logger.debug('Got dropbox request')
+        logger.debug(str(req))
         for uid in req['delta']['users']:
             # We need to respond quickly to the webhook request, 
             # so we do the actual work in a separate thread.
-            open('{}.lock'.format(uid), 'a').close()
+            fname = '{}/{}.lock'.format(settings.BASE_DIR, uid)
+            logger.debug('Try to create {}'.format(fname))
+            open(fname, 'a').close()
+            logger.debug('Created')
             # TODO: For more robustness, it's a good idea to add the work to a
             # django-rq and process the queue in a worker process.
 
