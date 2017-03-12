3d0677fe9ee75d0b09ada3647bf355c7e2fd7aa9
krnr <serova_s@land.ru>
2017-03-12 08:33:50 +0300

    Add ActionForm for custom action

---
+++ rugbystat/clippings/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from django.contrib.admin.helpers import ActionForm
+
+from .models import Source
+
+
+class SourceForm(ActionForm):
+    source = forms.ModelChoiceField(queryset=Source.objects.all())
