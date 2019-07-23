ce33f632699c9de9c96d5c9c7056a415e10f1b61
Yu.Lobanov <yu.lobanov@x5.ru>
2019-07-23 10:33:03 +0400

    [refactor] format main/views.py

--- rugbystat/main/views.py
+++ rugbystat/main/views.py
@@ -1,6 +1,6 @@
 from collections import Counter
 
-from django.db.models import Count
+from django.db.models import Count  # noqa
 from django.shortcuts import render
 
 from clippings.models import Document
@@ -10,24 +10,22 @@ from .forms import ClippingsForm
 
 
 def main_view(request):
-    return render(request, 'base.html')
+    return render(request, "base.html")
 
 
 def teams_view(request):
-    return render(request, 'teams.html')
+    return render(request, "teams.html")
 
 
 def persons_view(request):
     form = PersonForm()
-    return render(request, 'persons.html', {'form': form})
+    return render(request, "persons.html", {"form": form})
 
 
 def clippings_view(request):
-    params_mapper = {
-        'source_type': 'source__kind'
-    }
+    params_mapper = {"source_type": "source__kind"}
     form = ClippingsForm(request.GET)
-    count_qs = Document.objects.values_list('source__kind', flat=True)
+    count_qs = Document.objects.values_list("source__kind", flat=True)
     # qs = dict(
     #     Document.objects.values_list('source__kind')
     #     .annotate(Count('id')).order_by()
@@ -36,16 +34,13 @@ def clippings_view(request):
     total = sum(counter.values())
     null = counter[None]
 
-    filters = {params_mapper.get(k, k): v for k,v in request.GET.items() if v}
+    filters = {params_mapper.get(k, k): v for k, v in request.GET.items() if v}
     if filters:
         qs = Document.objects.not_deleted().filter(**filters)
     else:
         qs = Document.objects.none()
-    return render(request, 'documents.html', 
-                  {
-                      'form': form,
-                      'total': counter,
-                      'null': null,
-                      'sum_total': total,
-                      'qs': qs,
-                  })
+    return render(
+        request,
+        "documents.html",
+        {"form": form, "total": counter, "null": null, "sum_total": total, "qs": qs},
+    )
