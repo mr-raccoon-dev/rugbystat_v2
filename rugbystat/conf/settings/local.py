3535509fbe64a53dca2f78693b61254651bf68e3
Yu.Lobanov <yu.lobanov@x5.ru>
2019-07-23 10:31:14 +0400

    [devops] add .devcontainer

--- rugbystat/conf/settings/local.py
+++ rugbystat/conf/settings/local.py
@@ -27,7 +27,7 @@ TEMPLATES[0]['OPTIONS']['loaders'] = [
 INSTALLED_APPS += ('debug_toolbar', )
 MIDDLEWARE = ('debug_toolbar.middleware.DebugToolbarMiddleware', ) + MIDDLEWARE
 
-INTERNAL_IPS = ['127.0.0.1', '10.0.2.2', ]
+INTERNAL_IPS = ['127.0.0.1', '172.21.0.1', ]
 
 DEBUG_TOOLBAR_CONFIG = {
     'DISABLE_PANELS': [
