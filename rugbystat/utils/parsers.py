50fdfbe21c4de5919b22796cc8e64634eed2646a
krnr <ultrasnet@mail.ru>
2022-01-09 09:32:16 +0300

    rebase_me

--- rugbystat/utils/parsers.py
+++ rugbystat/utils/parsers.py
@@ -69,10 +69,9 @@ TEAM_SIMILAR_THRESHOLD = 60
 
 
 def parse_rosters(_, data):
-    text = data["input"]
-    for role, player in players_from_input(text):
+    for role, player in players_from_input(data["input"]):
         # find person
-        find_or_create_in_db(role, player, text)
+        find_or_create_in_db(role, player, data)
 
 
 def players_from_input(raw_text: str) -> t.Generator:
